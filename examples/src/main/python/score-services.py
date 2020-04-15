98329128ec9b03d7d0b2ed385d34704f8ed392a0
Luiz Junqueira <junqueira.luizcarlo@gmail.com>
2020-04-15 19:45:31 -0300

    add test aztk

--- examples/src/main/python/score-services.py
+++ examples/src/main/python/score-services.py
@@ -3,14 +3,17 @@ import aztk.spark
 from aztk.error import AztkError
 import datetime
 
+
+
+
 #UPDATE this section to set your secrets 
 secrets_confg = aztk.spark.models.SecretsConfiguration(
     service_principal=aztk.spark.models.ServicePrincipalConfiguration(
-        tenant_id="Update with AAD Directory ID",
-        client_id="Update with App Registration ID",
-        credential="Update with AAD app password",
-        batch_account_resource_id="Update with batch account resource Id",
-        storage_account_resource_id="",
+        tenant_id="ed35e03c-6f6e-4fd4-8367-e30e5fd6dd38",    #"Update with AAD Directory ID",
+        client_id="fbbb5259-f89e-4d01-ad38-73c78180520e",    #"Update with App Registration ID",
+        credential="k.:b3X:OE/I0hXGp4vBA5SnAO_:MtW8:",                           #"Update with AAD app password",
+        batch_account_resource_id="/subscriptions/f0aacc32-5eb2-4eae-a9b2-b510094e20f3/resourceGroups/cloud-shell-storage-eastus/providers/Microsoft.Batch/batchAccounts/test333",               #"Update with batch account resource Id",
+        storage_account_resource_id="/subscriptions/f0aacc32-5eb2-4eae-a9b2-b510094e20f3/resourceGroups/cloud-shell-storage-eastus/providers/Microsoft.Storage/storageAccounts/cs210033fff9c7dc349",
     ),
     #Optional ssh_pub_key
     #ssh_pub_key="../../rsa_key.pub" 
@@ -25,20 +28,24 @@ print(ROOT_PATH)
 client = aztk.spark.Client(secrets_confg)
 
 # list available clusters
-clusters = client.list_clusters()
+# clusters = client.list_clusters()
+clusters = client.cluster.list()
 
 # define a custom script
-custom_script = aztk.spark.models.CustomScript(
-    name="pythondependencies.sh",
-    script=os.path.join(ROOT_PATH, 'custom-scripts', 'pythondependencies.sh'),
-    run_on="all-nodes")
+# custom_script = aztk.spark.models.CustomScript(
+# custom_script = aztk.spark.models.ClusterConfiguration(
+#     name="pythondependencies.sh",
+#     script=os.path.join(ROOT_PATH, 'custom-scripts', 'pythondependencies.sh'),
+#     run_on="all-nodes")
 
 # define spark configuration
 spark_conf = aztk.spark.models.SparkConfiguration(
     spark_defaults_conf=os.path.join(ROOT_PATH, '.aztk', 'spark-defaults.conf'),
-    spark_env_sh=os.path.join(ROOT_PATH, '.aztk', 'config', 'spark-env.sh'),
-    core_site_xml=os.path.join(ROOT_PATH, '.aztk', 'config', 'core-site.xml'),
-    jars=[os.path.join(ROOT_PATH,  '.aztk','config', 'jars', jar) for jar in os.listdir(os.path.join(ROOT_PATH, '.aztk', 'jars'))]
+    spark_env_sh=os.path.join(ROOT_PATH, '.aztk', 'spark-env.sh'),
+    core_site_xml=os.path.join(ROOT_PATH, '.aztk', 'core-site.xml'),
+    jars=[os.path.join(ROOT_PATH,  '.aztk', 'jars', jar) 
+        for jar in os.listdir(os.path.join(ROOT_PATH, '.aztk', 'jars'))
+    ]
 )
 
 userconfig = aztk.spark.models.UserConfiguration(
@@ -50,11 +57,20 @@ userconfig = aztk.spark.models.UserConfiguration(
 toolkit=aztk.models.Toolkit(software="spark", version="2.3.0") 
 
 # configure my cluster
+# cluster_config = aztk.spark.models.ClusterConfiguration(
+#     cluster_id="myfirstcluster", #Cluster should not include capital letters
+#     size = 4,    
+#     vm_size="Standard_E4s_v3", #standard_g2 4cpu, 56Gib,  Standard_E8s_v3 : 8cores, 64Gb
+#     custom_scripts=[custom_script],
+#     spark_configuration=spark_conf,
+#     user_configuration=userconfig,
+#     toolkit=toolkit
+# )
+
 cluster_config = aztk.spark.models.ClusterConfiguration(
     cluster_id="myfirstcluster", #Cluster should not include capital letters
     size = 4,    
     vm_size="Standard_E4s_v3", #standard_g2 4cpu, 56Gib,  Standard_E8s_v3 : 8cores, 64Gb
-    custom_scripts=[custom_script],
     spark_configuration=spark_conf,
     user_configuration=userconfig,
     toolkit=toolkit
@@ -63,13 +79,19 @@ cluster_config = aztk.spark.models.ClusterConfiguration(
 # create a cluster, and wait until it is ready
 try:
     print("Cluster configured creating cluster now :", datetime.datetime.now())
-    cluster = client.create_cluster(cluster_config)
+    # cluster = client.create_cluster(cluster_config)
+
+    import ipdb; ipdb.set_trace()
+    # No such file or directory: './.aztk/config/spark-env.sh
+    cluster = client.cluster.create(cluster_config)
     
-    cluster = client.get_cluster(cluster_config.cluster_id)
+    # cluster = client.get_cluster(cluster_config.cluster_id)
+    cluster = client.cluster.get(cluster_config.cluster_id)
     print("getting cluster for", cluster_config.cluster_id)
     
     # The cluster will take 5 to 10 minutes to be ready depending on the size and availability in the batch account
-    cluster = client.wait_until_cluster_is_ready(cluster.id)
+    # cluster = client.wait_until_cluster_is_ready(cluster.id)
+    # cluster = client.cluster.wait(cluster.id, )
 except AztkError as e:
     print(e)
     sys.exit()
